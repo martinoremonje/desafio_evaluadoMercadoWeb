43d53a70231a386f96cb6b7ede1fd64de3e20dcf
martinoremonje <m.monjeorellana@gmail.com>
2024-04-30 17:50:56 -0400

    arreglando cosas no terminado

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const PORT = 3000;
 const app = express();
 const __dirname = path.resolve();
 
+const productos = ["banana", "cebollas", "lechuga", "papas", "pimenton", "tomate"];
+
 app.use(express.static('public'));
 
 app.set("view engine", "hbs");
@@ -18,22 +20,15 @@ app.engine("hbs", exphbs.engine({
 app.use("/bootstrap", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 
 app.get("/", (req,res)=>{
-    res.render("main", {layout: "main", frutas:  [
-        "banana",
-        "cebollas",
-        "lechuga",
-        "papas",
-        "pimenton",
-        "tomate"
-        ]})
+    res.render("main", {layout: "main", frutas: productos})
 });
 
 app.get("/:producto", (req, res) => {
     const { producto } = req.params;
-    const productos = ["banana", "cebollas", "lechuga", "papas", "pimenton", "tomate"];
+    
 
     if (!productos.includes(producto)) {
-        return res.status(404).send("Producto no encontrado");
+        return res.status(404).send("Tipico Error 404...");
     }
     res.render("Producto", {
         layout: "Producto",
